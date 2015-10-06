9716bec90ff187609b1670086cba7c64536955e9
c344081 <c344081@gmail.com>
2015-10-06 19:52:58 +0800

    Initial Commit

---
+++ Qdaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Qdaily
+//
+//  Created by Envy15 on 15/10/6.
+//  Copyright (c) 2015年 c344081. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
